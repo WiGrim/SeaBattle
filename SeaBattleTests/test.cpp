b469171bac14b6ff33cfddc41d36145804ca4175
WiGrim <rassoha04@gmail.com>
2026-02-19 13:20:22 +0700

    Add board test

--- SeaBattleTests/test.cpp
+++ SeaBattleTests/test.cpp
@@ -1,6 +1,7 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "Board.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BoardTest, BoardCreation)
+{
+    Board board(10);
 }
