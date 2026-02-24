c95b6312dd08a347f015800c27ce7c7549f2891b
WiGrim <rassoha04@gmail.com>
2026-02-24 12:05:45 +0700

    Add more ship related tests and some utility ones

--- SeaBattleTests/test.cpp
+++ SeaBattleTests/test.cpp
@@ -197,3 +197,86 @@ TEST(PlayerTest, AddMultipleShips)
     EXPECT_EQ(p.board.getCell(3, 0), CellState::Ship);
     EXPECT_EQ(p.board.getCell(3, 1), CellState::Ship);
 }
+
+TEST(ShipTest, ShipRegistersHit)
+{
+    Ship ship(0, 0, 3, Orientation::Horizontal);
+
+    EXPECT_TRUE(ship.registerHit(0, 0));
+    EXPECT_FALSE(ship.isDestroyed());
+}
+
+TEST(ShipTest, ShipDestroyedAfterAllHits)
+{
+    Ship ship(0, 0, 2, Orientation::Horizontal);
+
+    ship.registerHit(0, 0);
+    ship.registerHit(1, 0);
+
+    EXPECT_TRUE(ship.isDestroyed());
+}
+
+TEST(ShipTest, HitOutsideShipReturnsFalse)
+{
+    Ship ship(0, 0, 2, Orientation::Horizontal);
+
+    EXPECT_FALSE(ship.registerHit(5, 5));
+}
+
+TEST(BoardTest, ShootDestroysShip)
+{
+    Player p("Alice", 5);
+    p.addShip(0, 0, 2, Orientation::Horizontal);
+
+    EXPECT_FALSE(p.ships[0].isDestroyed());
+
+    p.board.shoot(0, 0);
+    p.board.shoot(1, 0);
+
+    EXPECT_TRUE(p.ships[0].isDestroyed());
+}
+
+TEST(BoardTest, CannotShootSameCellTwice)
+{
+    Board board(5);
+    board.placeShip(0, 0);
+
+    board.shoot(0, 0);
+
+    EXPECT_THROW(board.shoot(0, 0), std::logic_error);
+}
+
+TEST(GameTest, GameDetectsWinner)
+{
+    Game game("Alice", "Bob", 2);
+
+    game.player1.addShip(0, 0, 1, Orientation::Horizontal);
+    game.player2.addShip(1, 1, 1, Orientation::Horizontal);
+
+    game.currentPlayer = &game.player1;
+    game.opponent = &game.player2;
+
+    game.shootAtOpponent(1, 1);
+
+    EXPECT_TRUE(game.isGameOver());
+    EXPECT_EQ(game.getWinnerName(), "Alice");
+}
+
+TEST(PlayerTest, CannotExceedShipPool)
+{
+    Player p("Alice", 10);
+
+    p.addShip(0, 0, 4, Orientation::Horizontal);
+
+    EXPECT_THROW(
+        p.addShip(0, 2, 4, Orientation::Horizontal),
+        ShipPlaceError
+    );
+}
+
+TEST(GameTest, CannotStartGameUntilShipsPlaced)
+{
+    Game game("Alice", "Bob", 10);
+
+    EXPECT_THROW(game.start(), std::logic_error);
+}
